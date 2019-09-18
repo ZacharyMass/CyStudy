4aa033bd046ec92ab5c47f6f02ede7f2491baecb
Zach Gorman <gorman.zacharyj@gmail.com>
2019-09-18 15:41:52 -0500

    setting up an experiment for backend

---
+++ Experiments/Zach/android_mysql_tutorial/app/src/main/java/com/example/cystudy/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.cystudy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Hide Action Bar
+        getSupportActionBar().hide();
+    }
+
+    public void navigateToRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        // DO DATA VALIDATION, if good, login and navigate
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ Experiments/Zach/android_mysql_tutorial/app/src/main/java/com/example/cystudy/ui/classes/ClassesFragment.java
@@ -0,0 +1,39 @@
+package com.example.cystudy.ui.classes;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cystudy.R;
+
+public class ClassesFragment extends Fragment {
+
+    private ClassesViewModel mViewModel;
+
+    public static ClassesFragment newInstance() {
+        return new ClassesFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.classes_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(ClassesViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
