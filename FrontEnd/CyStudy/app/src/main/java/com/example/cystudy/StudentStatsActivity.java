4aa67a421699402a8e351b75abbb7f76fa997756
Brad Gannon <bgannon@iastate.edu>
2019-09-18 21:32:05 -0500

    Added Item Selected Listener to StudentStatsActivity.java

--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/StudentStatsActivity.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/StudentStatsActivity.java
@@ -3,8 +3,11 @@ package com.example.cystudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class StudentStatsActivity extends AppCompatActivity {
 
@@ -20,22 +23,58 @@ public class StudentStatsActivity extends AppCompatActivity {
         // Will be pulled from database
         String[] classes = {"CPRE 310", "STAT 330", "COM S 309", "COM S 321"};
         setContentView(R.layout.activity_student_stats);
-        Spinner spin = (Spinner) findViewById(R.id.classesDropdown);
+        Spinner spin = findViewById(R.id.classesDropdown);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, classes);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spin.setAdapter(adapter);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setOnItemSelectedListener(onItemSelectedListener1);
 
-        int minutesSpent = 238; // Total time spent studying for class (will be pulled from server/database
-
-        setTimeSpentStudying(minutesSpent);
     }
 
+    AdapterView.OnItemSelectedListener onItemSelectedListener1 =
+            new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    Spinner classSpinner = findViewById(R.id.classesDropdown);
+                    String selectedClass = classSpinner.getSelectedItem().toString(); // Figure out which class is selected
+
+                    String time;
+                    // Total time spent studying for classes (will be pulled from server/database)
+                    double accuracy; // Will be 
+
+                    TextView timeString = findViewById(R.id.timeSpentValue); // Access empty time slot on page
+
+                    // Apply minutes variable to appropriate class value
+                    if (selectedClass.matches("CPRE 310")) {
+                        int CPRE_minutesSpent = 238;
+                        time = setTimeSpentStudying(CPRE_minutesSpent);
+
+                    } else if (selectedClass.matches("STAT 330")) {
+                        int STAT_minutesSpent = 149;
+                        time = setTimeSpentStudying(STAT_minutesSpent);
+                    } else if (selectedClass.matches("COM S 309")) {
+                        int COMS309_minutesSpent = 300;
+                        time = setTimeSpentStudying(COMS309_minutesSpent);
+                    } else {
+                        int COMS321_minutesSpent = 101;
+                        time = setTimeSpentStudying(COMS321_minutesSpent);
+                    }
+
+                    timeString.setText(time);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {}
+            };
+
     /**
-     * Function to convert time to hours, minutes and set this text in Student Stats page
+     * Function to convert time to hours, minutes and return the String
      * @param minutes the total minutes spent on class
      */
-    public void setTimeSpentStudying(int minutes) {
-
+    public String setTimeSpentStudying ( int minutes){
+        int hours = minutes / 60; // Get hours
+        minutes = minutes - (hours * 60); // Get remainder of minutes
+        return hours + "h" + " " + minutes + "m";
     }
 }
