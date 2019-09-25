8b5e55137d0b8c9e922503f558e0aa363a391c71
Zachary Mass <zmass@iastate.edu>
2019-09-24 20:21:40 -0500

    Switched StudentStats to a fragment

--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/classes/StudentHomeFragment.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/classes/StudentHomeFragment.java
@@ -1,22 +1,18 @@
 package com.example.cystudy.ui.classes;
 
-import androidx.lifecycle.ViewModelProviders;
-
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.example.cystudy.MainActivity;
 import com.example.cystudy.R;
-import com.example.cystudy.StudentStatsActivity;
 
 public class StudentHomeFragment extends Fragment {
 
@@ -30,23 +26,17 @@ public class StudentHomeFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        Button button = inflater.inflate(R.layout.student_home_fragment, container, false).findViewById(R.id.yourStatsButton);
+
+        //Bind button to navigate to StudentStatsFragment
+        Button button = inflater.inflate(R.layout.fragment_student_home, container, false).findViewById(R.id.yourStatsButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), StudentStatsActivity.class);
-                startActivity(intent);
+                Navigation.findNavController(view).navigate(R.id.action_studentHomeFragment_to_studentStatsFragment);
             }
         });
 
-        return inflater.inflate(R.layout.student_home_fragment, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(ClassesViewModel.class);
-        // TODO: Use the ViewModel
+        return inflater.inflate(R.layout.fragment_student_home, container, false);
     }
 
 }
