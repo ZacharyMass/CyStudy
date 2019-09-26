ad5ae18707cc8224b954b59c671cf9920b4cc928
Brad Gannon <bgannon@iastate.edu>
2019-09-26 12:57:28 -0500

    Working on Volley tutorial in LoginActivity class

--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/LoginActivity.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/LoginActivity.java
@@ -3,13 +3,34 @@ package com.example.cystudy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.AccessController;
 import java.util.Objects;
 
+import static java.security.AccessController.getContext;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private String url = ""; // This will need to be filled in once server guys let us know
+
+    private EditText usernameSpace = findViewById(R.id.email_login);
+    private EditText passwordSpace = findViewById(R.id.password_login);
+    private Button btn = findViewById(R.id.login_button);
+    public static String class1, class2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +47,68 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-        // Data validation (check if not empty) and get string from EditText values
-        EditText usernameSpace = findViewById(R.id.email_login);
-        EditText passwordSpace = findViewById(R.id.password_login);
-        String username = usernameSpace.getText().toString();
-        String password = passwordSpace.getText().toString();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+    }
 
-        // if (!username.matches("") && !password.matches("")) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        // }
+    private void loginUser(){
+
+        final String username = usernameSpace.getText().toString().trim();
+        final String password = passwordSpace.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this,response,Toast.LENGTH_LONG).show();
+                        parseData(response);
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("username",username);
+                params.put("password",password);
+
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    public void parseData(String response) {
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            if (jsonObject.getString("status").equals("true")) {
+                JSONArray dataArray = jsonObject.getJSONArray("data");
+                for (int i = 0; i < dataArray.length(); i++) {
+
+                    JSONObject dataobj = dataArray.getJSONObject(i);
+                    firstName = dataobj.getString("name");
+                    hobby = dataobj.getString("hobby");
+                }
+
+                Intent intent = new Intent(MainActivity.this,HobbyActivity.class);
+                startActivity(intent);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 }
