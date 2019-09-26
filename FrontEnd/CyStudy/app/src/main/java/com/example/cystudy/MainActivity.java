85aebed7939cd579e9b38b216eb9a45fc9c3c9bc
Zachary Mass <zmass@iastate.edu>
2019-09-26 17:22:45 -0500

    Added Menu Options

--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/MainActivity.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.cystudy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +18,25 @@ public class MainActivity extends AppCompatActivity {
 
         // Hide action bar
         getSupportActionBar().hide();
+
+//        final BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+//        bottomNavigationView.setOnNavigationItemSelectedListener(
+//                new BottomNavigationView.OnNavigationItemSelectedListener() {
+//                    @Override
+//                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                        switch (menuItem.getItemId()) {
+//                            case R.id.action_home:
+//                                break;
+//                            case R.id.action_classes:
+//                                break;
+//                            case R.id.action_stats:
+//                                break;
+//                        }
+//                        return false;
+//                    }
+//                }
+//        );
     }
+
+
 }
