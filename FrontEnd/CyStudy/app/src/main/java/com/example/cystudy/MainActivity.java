fd4d28f90013d4eeb41c61622a6e5c28161818f4
Zachary Mass <zmass@iastate.edu>
2019-09-26 21:58:42 -0500

    Implemented navigation via bottom navigation bar

--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/MainActivity.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.cystudy;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static androidx.navigation.ui.NavigationUI.setupWithNavController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,24 +22,36 @@ public class MainActivity extends AppCompatActivity {
         // Hide action bar
         getSupportActionBar().hide();
 
-//        final BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
-//        bottomNavigationView.setOnNavigationItemSelectedListener(
-//                new BottomNavigationView.OnNavigationItemSelectedListener() {
-//                    @Override
-//                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-//                        switch (menuItem.getItemId()) {
-//                            case R.id.action_home:
-//                                break;
-//                            case R.id.action_classes:
-//                                break;
-//                            case R.id.action_stats:
-//                                break;
-//                        }
-//                        return false;
-//                    }
-//                }
-//        );
+        // Handle Navigation between fragments with Bottom Nav Bar
+        final BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        final NavController navController = Navigation.findNavController(this, R.id.fragment);
+        setupWithNavController(bottomNavigationView, navController);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        if (navController.getCurrentDestination().getId() == R.id.studentStatsFragment) {
+                            switch (menuItem.getItemId()) {
+                                case R.id.action_home:
+                                    navController.navigate(R.id.action_studentStatsFragment_to_studentHomeFragment);
+                                    break;
+                                case R.id.action_classes:
+                                    break;
+                            }
+                        }
+                        else if (navController.getCurrentDestination().getId() == R.id.studentHomeFragment) {
+                            switch (menuItem.getItemId()) {
+                                case R.id.action_stats:
+                                    navController.navigate(R.id.action_studentHomeFragment_to_studentStatsFragment);
+                                    break;
+                                case R.id.action_classes:
+                                    break;
+                            }
+                        }
+                        return true;
+                    }
+                }
+        );
     }
-
 
 }
