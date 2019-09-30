6e0855a6bbb2fa2943fd659abe11a20d21dfd33f
Zachary Mass <zmass@iastate.edu>
2019-09-30 16:39:58 -0500

    Fixed user format issues

--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/RegisterActivity.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/RegisterActivity.java
@@ -4,22 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -49,14 +44,19 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void register(View view) {
-
+        // Define Views
         final EditText email = findViewById(R.id.email_register);
         final EditText pass = findViewById(R.id.password_register);
         final EditText passConf = findViewById(R.id.password_confirm_register);
         final Spinner role = findViewById(R.id.role_spinner);
 
         // DO DATA VALIDATION, if good, login and navigate, else Toast and return
-        if(pass.equals(null))
+        if(email.getText().length() == 0)
+        {
+            Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(pass.getText().length() == 0)
         {
             Toast.makeText(this, "Enter Password", Toast.LENGTH_SHORT).show();
             return;
@@ -67,22 +67,21 @@ public class RegisterActivity extends AppCompatActivity {
             Toast.makeText(this, "Passwords Do Not Match", Toast.LENGTH_SHORT).show();
             return;
         }
-        if(email.equals(null))
-        {
-            Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show();
-            return;
-        }
 
         // Establish Queue
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://coms-309-jr-7.misc.iastate.edu:8080/add-user?username=insertUsername&pass=insertPassword&role=studentOrProfessor";
+        String url = "http://coms-309-jr-7.misc.iastate.edu:8080/add-user";
         StringRequest postRequest = new StringRequest(Request.Method.POST, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         Log.d("Response", response);
+
+                        // Toast User Creation
                         Toast toast = Toast.makeText(getApplicationContext(), "User Created", Toast.LENGTH_SHORT);
                         toast.show();
+
+                        // Navigate Home
                         Intent intent = new Intent(getBaseContext(), MainActivity.class);
                         startActivity(intent);
                     }
@@ -90,6 +89,7 @@ public class RegisterActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        // Show Error
                         String response = "Error Occurred: " + error.getMessage();
                         Log.d("Error.Response", response);
                         Toast toast = Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG);
@@ -100,12 +100,16 @@ public class RegisterActivity extends AppCompatActivity {
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
                 params.put("username", email.getText().toString());
-                params.put("password", pass.getText().toString());
-                params.put("role", role.getSelectedItem().toString());
+                params.put("pass", pass.getText().toString());
+                params.put("role", role.getSelectedItem().toString().toLowerCase());
+
+                Log.d("Tag", params.toString());
 
                 return params;
             }
         };
+
+        // Send to Server
         queue.add(postRequest);
     }
 }
--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/classes/StudentHomeFragment.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/classes/StudentHomeFragment.java
@@ -1,28 +1,16 @@
 package com.example.cystudy.ui.classes;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.example.cystudy.MainActivity;
 import com.example.cystudy.R;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class StudentHomeFragment extends Fragment {
 
