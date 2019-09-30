c7197c6fd272e950becc78e9e9583291100dacde
Brad Gannon <bgannon@iastate.edu>
2019-09-30 12:01:54 -0500

    Running into issues with connecting to server

--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/LoginActivity.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/LoginActivity.java
@@ -4,8 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class LoginActivity extends AppCompatActivity {
 
--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/classes/StudentHomeFragment.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/classes/StudentHomeFragment.java
@@ -1,13 +1,6 @@
 package com.example.cystudy.ui.classes;
 
-import android.content.Context;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,64 +8,57 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.cystudy.App.AppController;
 import com.example.cystudy.R;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 import static com.example.cystudy.App.AppController.TAG;
 
 public class StudentHomeFragment extends Fragment {
+
+    // public static String url = "http://coms-309-jr-7.misc.iastate.edu:8080/get-users";
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
         //Bind button to navigate to StudentStatsFragment
-        View v = inflater.inflate(R.layout.fragment_student_home, container, false);
+        final View v = inflater.inflate(R.layout.fragment_student_home, container, false);
         Button button = v.findViewById(R.id.yourStatsButton);
         button.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.studentStatsFragment, null));
         final TextView classList = v.findViewById(R.id.classesText); // Will be replaced by class text
 
-        String url = "http://coms-309-jr-7.misc.iastate.edu:3306/cystudy/user/get-users";
+        // Tag used to cancel the request
+        String tag_json_obj ="json_obj_req";
+        String url ="https://api.androidhive.info/volley/person_object.json";
 
-        Context mContext = getContext();
-
-        RequestQueue requestQueue = Volley.newRequestQueue(mContext);
-
-        JsonObjectRequest getJSONClasses = new JsonObjectRequest(Request.Method.GET, url, null,
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                url,null,
                 new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        classList.setText(response.toString());
-                    }
-                },
-                new Response.ErrorListener() {
             @Override
-                    public void onErrorResponse(VolleyError error) {
-                classList.setText(error.toString());
+                    public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
             }
-                });
-
-        requestQueue.add(getJSONClasses);
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG,"Error: "+ error.getMessage());
+            }
+        });
+        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
 
         return v;
     }
-
 }
-
