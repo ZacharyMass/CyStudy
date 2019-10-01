83d3909120bd23544842a499e428dde498ad70c3
Brad Gannon <bgannon@iastate.edu>
2019-09-30 20:52:06 -0500

    Fixed another merge conflict in LruBitmapCache

--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/LoginActivity.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/LoginActivity.java
@@ -48,16 +48,6 @@ public class LoginActivity extends AppCompatActivity {
         EditText passwordSpace = findViewById(R.id.password_login);
         final String username = usernameSpace.getText().toString();
         String password = passwordSpace.getText().toString();
-
-
-<<<<<<< HEAD
-        checkValidUsername(username);
-=======
-        // if (!username.matches("") && !password.matches("")) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        // }
->>>>>>> 959198f8234ece215b76c322bea546162a8e6d1c
     }
 
     private void checkValidUsername(final String username) {
--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/net_utils/LruBitmapCache.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/net_utils/LruBitmapCache.java
@@ -1,56 +1,38 @@
 package com.example.cystudy.net_utils;
 
-<<<<<<< HEAD
 import android.graphics.Bitmap;
 import android.util.LruCache;
 
 import com.android.volley.toolbox.ImageLoader.ImageCache;
-public class LruBitmapCache extends LruCache<String, Bitmap>
-        implements
-        ImageCache {
-    public static int getDefaultLruCacheSize() {
-        final int maxMemory = (int) (Runtime.getRuntime().maxMemory()
-                / 1024);
-        final int cacheSize = maxMemory / 8;
-=======
-import com.android.volley.toolbox.ImageLoader.ImageCache;
 
-import android.graphics.Bitmap;
-import android.util.LruCache;
+        public class LruBitmapCache extends LruCache<String, Bitmap> implements ImageCache {
+            public static int getDefaultLruCacheSize() {
+                final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+                final int cacheSize = maxMemory / 8;
 
-public class LruBitmapCache extends LruCache<String, Bitmap> implements ImageCache {
-    public static int getDefaultLruCacheSize() {
-        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
-        final int cacheSize = maxMemory / 8;
+                return cacheSize;
+            }
 
->>>>>>> 959198f8234ece215b76c322bea546162a8e6d1c
-        return cacheSize;
-    }
+            public LruBitmapCache() {
+                this(getDefaultLruCacheSize());
+            }
 
-    public LruBitmapCache() {
-        this(getDefaultLruCacheSize());
-    }
+            public LruBitmapCache(int sizeInKiloBytes) {
+                super(sizeInKiloBytes);
+            }
 
-    public LruBitmapCache(int sizeInKiloBytes) {
-        super(sizeInKiloBytes);
-    }
+            @Override
+            protected int sizeOf(String key, Bitmap value) {
+                return value.getRowBytes() * value.getHeight() / 1024;
+            }
 
-    @Override
-    protected int sizeOf(String key, Bitmap value) {
-        return value.getRowBytes() * value.getHeight() / 1024;
-    }
+            @Override
+            public Bitmap getBitmap(String url) {
+                return get(url);
+            }
 
-    @Override
-    public Bitmap getBitmap(String url) {
-        return get(url);
-    }
-
-    @Override
-    public void putBitmap(String url, Bitmap bitmap) {
-        put(url, bitmap);
-    }
-}
-<<<<<<< HEAD
-=======
-
->>>>>>> 959198f8234ece215b76c322bea546162a8e6d1c
+            @Override
+            public void putBitmap(String url, Bitmap bitmap) {
+                put(url, bitmap);
+            }
+        }
