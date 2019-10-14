25b2aa38940f9461acf65ef7ab21f7783ad32dc3
Zach Gorman <gorman.zacharyj@gmail.com>
2019-10-14 15:38:04 -0500

    added password encoding

--- BackEnd/src/main/java/com/jr7/cystudy/service/UserService.java
+++ BackEnd/src/main/java/com/jr7/cystudy/service/UserService.java
@@ -2,6 +2,10 @@ package com.jr7.cystudy.service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.jr7.cystudy.model.User;
@@ -13,8 +17,16 @@ public class UserService {
     @Autowired
     private UserRepository UserRepository;
 
-//    @Autowired
-//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     public List<User> getUsers() {
         return UserRepository.findAll();
@@ -33,10 +45,23 @@ public class UserService {
         }
     }
 
-    public void save(User user) {
+    public void save(User user) throws Exception{
+
+        if(getUserByName(user.getUsername()) != null){
+            throw new Exception("The username " + user.getUsername() + " is already being used.");
+        }
+
         user.setUsername(user.getUsername());
-        user.setPassword(user.getPassword());
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setRole(user.getRole());
         UserRepository.save(user);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
