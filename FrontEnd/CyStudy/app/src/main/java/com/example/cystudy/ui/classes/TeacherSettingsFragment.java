f696243b19bd44ef3b4c23046aa9464d513794af
unknown <bgannon@iastate.edu>
2019-10-15 19:01:45 -0500

    Added nav map for teacher fragments

---
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/classes/TeacherSettingsFragment.java
@@ -0,0 +1,26 @@
+package com.example.cystudy.ui.classes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.cystudy.R;
+
+public class TeacherSettingsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        final View v = inflater.inflate(R.layout.fragment_teacher_settings, container, false);
+
+        return v;
+    }
+}
