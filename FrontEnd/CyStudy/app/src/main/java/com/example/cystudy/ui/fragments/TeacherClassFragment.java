b4359c69fad5ac2b757f2ac57e6b1fa44dc3bc63
unknown <bgannon@iastate.edu>
2019-10-21 18:50:16 -0500

    All merge conflicts on my branch fixed

--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/fragments/TeacherClassFragment.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/fragments/TeacherClassFragment.java
@@ -1,4 +1,4 @@
-package com.example.cystudy.ui.classes;
+package com.example.cystudy.ui.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,6 +14,7 @@ import androidx.navigation.NavController;
 
 import com.example.cystudy.MainActivity;
 import com.example.cystudy.R;
+import com.example.cystudy.ui.classes.TeacherHomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import static androidx.navigation.ui.NavigationUI.setupWithNavController;
--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/fragments/TeacherHomeFragment.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/fragments/TeacherHomeFragment.java
@@ -1,4 +1,4 @@
-package com.example.cystudy.ui.classes;
+package com.example.cystudy.ui.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
