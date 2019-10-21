01e7ea49764cb7767057992c9e54cba279a25fb1
Zach Gorman <gorman.zacharyj@gmail.com>
2019-10-20 22:42:24 -0500

    added ability to save terms, getTerms, getTopics, and check if a topic exists

--- BackEnd/src/main/java/com/jr7/cystudy/service/TermsService.java
+++ BackEnd/src/main/java/com/jr7/cystudy/service/TermsService.java
@@ -1,8 +1,12 @@
 package com.jr7.cystudy.service;
 
+import com.jr7.cystudy.model.Terms;
 import com.jr7.cystudy.repository.TermsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TermsService {
@@ -10,5 +14,28 @@ public class TermsService {
   @Autowired
   private TermsRepository TermsRepository;
 
+  public void save(Terms t){
+    TermsRepository.save(t);
+  }
 
+  public List<Terms> getTermsByClass(String className){
+    return TermsRepository.getTermsByClassName(className);
+  }
+
+  public boolean topicExists(String className, String topic){
+    ArrayList<String> topics = getTopics(className);
+    return topics.contains(topic);
+  }
+
+  public ArrayList<String> getTopics(String className){
+
+    List<Terms> termsInClass = getTermsByClass(className);
+    ArrayList<String> topics = new ArrayList<>();
+
+    for(Terms t : termsInClass){
+      if(topics.contains(t.getTopic())) continue;
+      topics.add(t.getTopic());
+    }
+    return topics;
+  }
 }
