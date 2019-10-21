f433abe3947770532c8de5f36192e816eb86e481
Zach Gorman <gorman.zacharyj@gmail.com>
2019-10-20 22:41:31 -0500

    added function getTermsByClassName

--- BackEnd/src/main/java/com/jr7/cystudy/repository/TermsRepository.java
+++ BackEnd/src/main/java/com/jr7/cystudy/repository/TermsRepository.java
@@ -5,8 +5,11 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jr7.cystudy.model.Terms;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 @Repository
 @Transactional
 public interface TermsRepository extends JpaRepository<Terms, String>{
 
+  List<Terms>  getTermsByClassName(String className);
 }
