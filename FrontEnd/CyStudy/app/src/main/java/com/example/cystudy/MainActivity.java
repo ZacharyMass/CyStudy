b5322a7af3caca276e3a8f1bf6e5c342048ec9d0
unknown <bgannon@iastate.edu>
2019-10-21 19:43:21 -0500

    Switching to massBranch to see if emulator working

--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/MainActivity.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/MainActivity.java
@@ -1,30 +1,15 @@
 package com.example.cystudy;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.Objects;
 
 import static androidx.navigation.ui.NavigationUI.setupWithNavController;
 
