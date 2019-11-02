f2c2c1eb455f3325570a6727cc0290e7542123b8
Zach Gorman <gorman.zacharyj@gmail.com>
2019-11-01 22:28:28 -0500

    javadocs

--- BackEnd/src/main/java/com/jr7/cystudy/controller/TermsController.java
+++ BackEnd/src/main/java/com/jr7/cystudy/controller/TermsController.java
@@ -11,17 +11,42 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Controller for all things having to do with Terms objects
+ *
+ * @see Terms
+ * @see TermsService
+ * @see com.jr7.cystudy.repository.TermsRepository
+ * @see ClassesService
+ * @author Zach Gorman
+ */
 @RestController
 public class TermsController {
 
+  /**
+   * @see TermsService
+   */
   @Autowired
   private TermsService TermsService;
 
+  /**
+   * @see ClassesService
+   */
   @Autowired
   private ClassesService ClassesService;
 
+  /**
+   * Logger for debugging
+   */
   private final Logger log = LoggerFactory.getLogger(TermsController.class);
 
+  /**
+   * Endpoint to add a term to a class. Checks if the class exists, and if it does, adds the card. We don't check if the
+   * topic exists, b/c it either didn't, or does now with this addition. That will be fixed... later...
+   *
+   * @param t Terms object containing at least term, answer, and classname
+   * @return String saying that the class doesn't exist, or was succesfully added.
+   */
   @PostMapping(path="/add-card")
   public @ResponseBody String addFlashCard(@RequestBody Terms t){
 
@@ -38,11 +63,25 @@ public class TermsController {
     }
   }
 
+  /**
+   * Endpoint to get all of the terms belonging to a specific class
+   * @param className name of the class to get the terms for
+   * @return List of Terms objects (JSON when called from endpoint).
+   * @see TermsService
+   */
   @GetMapping(path="/get-terms-by-class")
   public @ResponseBody List<Terms> getTermsByClass(@RequestParam String className){
     return TermsService.getTermsByClass(className);
   }
 
+  /**
+   * Endpoint to get all of the terms for a specific topic in a specific class. Class name must be specified b/c it is
+   * possible for multiple classes to have topics of the same name (e.g. Chapter 1, etc.).
+   *
+   * @param className name of the class the topic belongs to
+   * @param topic name of the topic
+   * @return null if the topic doesn't exist, or the List of Terms objects (as a JSON if called via HTTP)
+   */
   @GetMapping(path="get-terms-by-topic")
   public @ResponseBody List<Terms> getTermsByTopic(@RequestParam String className, @RequestParam String topic){
 
@@ -62,11 +101,23 @@ public class TermsController {
     return termsByTopic;
   }
 
+  /**
+   * Endpoint to get all of the topics for a specific class.
+   *
+   * @param className name of the class to ghet the topics of
+   * @return ArrayList of Strings containing all of the topics
+   */
   @GetMapping(path="get-class-topics")
   public @ResponseBody ArrayList<String> getClassTopics(@RequestParam String className){
     return TermsService.getTopics(className);
   }
 
+  /**
+   * Endpoint to delete a specific flashcard that matches the specified parameters
+   *
+   * @param t Terms object containing at least classname, term, and answer
+   * @return String defined in {@link TermsService}
+   */
   @PostMapping(path="/delete-card", produces="application/json", consumes="application/json")
   public @ResponseBody String deleteCard(@RequestBody Terms t){
     return TermsService.deleteCard(t.getTerm(), t.getAnswer(), t.getClassName());
--- BackEnd/src/main/java/com/jr7/cystudy/service/TermsService.java
+++ BackEnd/src/main/java/com/jr7/cystudy/service/TermsService.java
@@ -8,25 +8,57 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Logic behind {@link com.jr7.cystudy.controller.TermsController}
+ *
+ * @author Zach Gorman
+ */
 @Service
 public class TermsService {
 
+  /**
+   * @see TermsRepository
+   */
   @Autowired
   private TermsRepository TermsRepository;
 
+  /**
+   * Saves a Terms object in the database.
+   *
+   * @param t a Term object to save in the database
+   */
   public void save(Terms t){
     TermsRepository.save(t);
   }
 
+  /**
+   * Gets all of the terms registered in the specified class, and returns them as a list.
+   *
+   * @param className String of the name of the class to get the terms for
+   * @return List of {@link Terms} objects
+   */
   public List<Terms> getTermsByClass(String className){
     return TermsRepository.getTermsByClassName(className);
   }
 
+  /**
+   * Function to check that a topic in a class exists.
+   *
+   * @param className String of the class that the topic is/isn't a part of
+   * @param topic String of the name of the topic we're checking exists or not
+   * @return boolean based on if topic already exists in the class or not
+   */
   public boolean topicExists(String className, String topic){
     ArrayList<String> topics = getTopics(className);
     return topics.contains(topic);
   }
 
+  /**
+   * Gets all topics in a specific class, and returns them as an ArrayList of Strings
+   *
+   * @param className String of the name of the class that you want the topics for
+   * @return ArrayList of Strings of all topics registered for the class.
+   */
   public ArrayList<String> getTopics(String className){
 
     List<Terms> termsInClass = getTermsByClass(className);
@@ -39,6 +71,14 @@ public class TermsService {
     return topics;
   }
 
+  /**
+   * Function to remove a card from the database
+   *
+   * @param term term portion of the Term object to remove
+   * @param answer answer portion of the Term object to remove
+   * @param className className portion of the Term object to remove
+   * @return String saying "If card existed, or multiple of card existed, it/they are now deleted."
+   */
   public String deleteCard(String term, String answer, String className){
     TermsRepository.deleteCard(term, answer, className);
     return "If card existed, or multiple of card existed, it/they are now deleted.";
