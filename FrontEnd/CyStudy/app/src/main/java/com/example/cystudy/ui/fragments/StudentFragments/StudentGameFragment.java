04886cdbf5d6525b50ad1ba75e19ce0b7c1b20d6
Zachary Mass <zmass@iastate.edu>
2019-11-04 21:08:19 -0600

    Initial Websocket implementation

--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/fragments/StudentFragments/StudentGameFragment.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/fragments/StudentFragments/StudentGameFragment.java
@@ -1,9 +1,12 @@
 package com.example.cystudy.ui.fragments.StudentFragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,7 +14,17 @@ import androidx.fragment.app.Fragment;
 
 import com.example.cystudy.R;
 
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class StudentGameFragment extends Fragment {
+
+    private WebSocketClient cc;
 
     public StudentGameFragment() {
         // Required empty public constructor
@@ -23,6 +36,40 @@ public class StudentGameFragment extends Fragment {
 
         //Inflate view
         View v = inflater.inflate(R.layout.fragment_game, container, false);
+
+        Draft[] drafts = {new Draft_6455()};
+        String w = "ws://10.26.13.93:8080/websocket/";
+
+        try {
+            Log.d("Socket:", "Trying socket");
+            cc = new WebSocketClient(new URI(w), (Draft) drafts[0]) {
+                @Override
+                public void onMessage(String message) {
+                    Log.d("", "run() returned: " + message);
+                    Toast t = Toast.makeText(getContext(), message, Toast.LENGTH_LONG);
+                    t.show();
+                }
+
+                @Override
+                public void onOpen(ServerHandshake handshake) {
+                    Log.d("OPEN", "run() returned: " + "is connecting");
+                }
+
+                @Override
+                public void onClose(int code, String reason, boolean remote) {
+                    Log.d("CLOSE", "onClose() returned: " + reason);
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    Log.d("Exception:", e.toString());
+                }
+            };
+        } catch (URISyntaxException e) {
+            Log.d("Exception:", e.getMessage().toString());
+            e.printStackTrace();
+        }
+        cc.connect();
 
         return v;
     }
