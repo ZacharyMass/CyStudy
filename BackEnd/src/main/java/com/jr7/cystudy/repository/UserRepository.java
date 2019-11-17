72bdd321d9aed72fac130e49969482311fb2bbe0
Zach Gorman <gorman.zacharyj@gmail.com>
2019-11-16 23:19:40 -0600

    fixes based on newly installed linter to match Google's Java Format

--- BackEnd/src/main/java/com/jr7/cystudy/repository/UserRepository.java
+++ BackEnd/src/main/java/com/jr7/cystudy/repository/UserRepository.java
@@ -1,22 +1,25 @@
 package com.jr7.cystudy.repository;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import com.jr7.cystudy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Connection to usr table in the MySQL database.
+ *
  * @author Zach Gorman
  */
 @Repository
 @Transactional
-public interface UserRepository extends JpaRepository<User, String>{
+public interface UserRepository extends JpaRepository<User, String> {
 
-    /**
-     * @see com.jr7.cystudy.service.UserService
-     * @param  username name of the user to get the information of
-     * @return user     {@link User}
-     */
-    User getUserByUsername(String username);
+  /**
+   * Query database to get a User's details based on their username.
+   *
+   * @see com.jr7.cystudy.service.UserService
+   * @param username name of the user to get the information of
+   * @return user {@link User}
+   */
+  User getUserByUsername(String username);
 }
