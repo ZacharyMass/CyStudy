bb48a0e4dcee309b9ea20404a4cfb66e767c1fcb
unknown <bgannon@iastate.edu>
2019-11-18 15:30:14 -0600

    Quick commit before pulling backend changes

--- FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/fragments/AdminFragments/AdminManageTeachersFragment.java
+++ FrontEnd/CyStudy/app/src/main/java/com/example/cystudy/ui/fragments/AdminFragments/AdminManageTeachersFragment.java
@@ -1,24 +1,63 @@
 package com.example.cystudy.ui.fragments.AdminFragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.cystudy.R;
+import com.example.cystudy.RecyclerViewAdapaters.RecyclerViewAdapter;
+import com.example.cystudy.ui.fragments.TeacherFragments.TeacherClassFragment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class AdminManageTeachersFragment extends Fragment {
+
+    private View v;
+    ArrayList<String> teachersL = new ArrayList<>();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
+        teachersL.clear(); // Clear on creation to prevent duplication of RecyclerView objects
+
         View v = inflater.inflate(R.layout.fragment_admin_manage_teachers, container, false);
 
+        pullTeachers();
+
         return v;
+    }
+
+    public void pullTeachers() {
+
+        String URL = "http://coms-309-jr-7.misc.iastate.edu:8080/get-users-by-role";
+
+
     }
 }
