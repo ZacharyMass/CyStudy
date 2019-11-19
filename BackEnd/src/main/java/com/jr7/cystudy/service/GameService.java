588b7b9e798c265f5bd6567c04bf4b1a7e0e0001
Zach Gorman <gorman.zacharyj@gmail.com>
2019-11-18 18:17:38 -0600

    added javadoc

--- BackEnd/src/main/java/com/jr7/cystudy/service/GameService.java
+++ BackEnd/src/main/java/com/jr7/cystudy/service/GameService.java
@@ -2,17 +2,23 @@ package com.jr7.cystudy.service;
 
 import com.jr7.cystudy.model.Game;
 import com.jr7.cystudy.model.Terms;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class GameService {
 
   @Autowired private TermsService termsService;
 
-  public List<Terms> getQuestions(Game g, String className){
+  /**
+   * Gets the questions based on the classname by querying the database.
+   *
+   * @param g the current game
+   * @param className the name of the class to get the terms for
+   * @see TermsService
+   */
+  public List<Terms> getQuestions(Game g, String className) {
 
     g.questions = termsService.getTermsByClass(className);
     return g.questions;
