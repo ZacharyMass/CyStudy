546ab4a7a95f6fdea608df727580c98052c9544b
Zach Gorman <gorman.zacharyj@gmail.com>
2019-11-18 18:13:30 -0600

    idk hopefully this new socket stuff works

--- BackEnd/src/main/java/com/jr7/cystudy/sockets/Server.java
+++ BackEnd/src/main/java/com/jr7/cystudy/sockets/Server.java
@@ -1,16 +1,23 @@
 package com.jr7.cystudy.sockets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+
+import com.jr7.cystudy.model.Game;
+import com.jr7.cystudy.model.Terms;
+import com.jr7.cystudy.service.GameService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@ServerEndpoint("/websocket/{username}")
+@ServerEndpoint("/websocket/{class_name}/{username}")
 @Component
 public class Server {
 
@@ -18,22 +25,44 @@ public class Server {
   private static Map<Session, String> sessionUsernameMap = new HashMap<>();
   private static Map<String, Session> usernameSessionMap = new HashMap<>();
 
+  private static Game g = new Game();
+  @Autowired GameService gameService;
+
   private final Logger logger = LoggerFactory.getLogger(Server.class);
 
   /*TODO
    * - Check for same username entering game
-   * - Check for more than 2 players
    * - multiple threads for other groups of 2 playing
    */
+  /**
+   * What to do when the websocket is opened.
+   *
+   * @param session session info
+   * @param username username of the user joining the session
+   */
   @OnOpen
-  public void onOpen(Session session, @PathParam("username") String username) throws IOException {
+  public void onOpen(Session session, @PathParam("class_name") String cName, @PathParam("username") String username)
+      throws IOException {
 
     // get session and websocket connection
     logger.info("Entered onOpen in com.jr7.cystudy.sockets.Server");
 
-    // if(sessionUsernameMap.containsKey(username))
+    // Not super sure this is worth implementing since usernames should be unique.
+    // Plus, can't find a good example of rejecting an incoming connection this way.
+    // if(sessionUsernameMap.containsKey(username)){ }
+
+    if (sessionUsernameMap.size() >= 2) {
+      broadcast("This game already has 2 players, and another tried to join.");
+      onClose(session);
+    }
+
+    if(sessionUsernameMap.isEmpty()) g.player1 = username;
+    else g.player2 = username;
+
     sessionUsernameMap.put(session, username);
     usernameSessionMap.put(username, session);
+
+    gameService.getQuestions(g, cName);
 
     String message = "User: " + username + " has Joined the Game";
     broadcast(message);
@@ -52,10 +81,11 @@ public class Server {
         });
   }
 
-  /*TODO
-   * - Population of terms and buttons
-   * - Taking in true and false
-   * - Taking in time spent on game
+  /**
+   * What happens when a message is sent to the socket.
+   *
+   * @param session session info
+   * @param message string version of the message being sent
    */
   @OnMessage
   public void onMessage(Session session, String message) throws IOException {
@@ -64,10 +94,66 @@ public class Server {
     logger.info("Entered into Message: Got Message:" + message);
     String username = sessionUsernameMap.get(session);
 
-    broadcast(username + ": " + message);
+    if( message.equalsIgnoreCase("true")  ||
+        message.equalsIgnoreCase("false") ||
+        message.equalsIgnoreCase("start") ){
+
+      if(username.equalsIgnoreCase(g.player1)) sendTerms(g.round, g.player2);
+      else if(username.equalsIgnoreCase(g.player2)) sendTerms(g.round, g.player1);
+      else sendTerms();
+
+      if(message.equalsIgnoreCase("true")){
+        if(username.equalsIgnoreCase(g.player1)) g.p1Correct++;
+        if(username.equalsIgnoreCase(g.player2)) g.p2Correct++;
+      }
+      g.round++;
+    }
+    else{
+      broadcast(username + ": " + message);
+    }
   }
 
-  // TODO
+  private static void sendTerms() throws IOException {
+
+    List<Terms> round_terms = new ArrayList<>();
+    for(int i = 0; i < 5; i++){
+      round_terms.add(g.questions.get(i));
+    }
+
+    sessionUsernameMap.forEach(
+      (session, username) -> {
+        synchronized (session) {
+          try {
+            session.getBasicRemote().sendObject(round_terms);
+          } catch (IOException | EncodeException e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    );
+  }
+
+  private static void sendTerms(int round, String uname) throws IOException {
+
+    int firstCardIdx = round*5;
+    List<Terms> round_terms = new ArrayList<>();
+    for(int i = firstCardIdx; i <= firstCardIdx+5; i++){
+      round_terms.add(g.questions.get(i));
+    }
+
+    try{
+      usernameSessionMap.get(uname).getBasicRemote().sendObject(round_terms);
+    }
+    catch(IOException | EncodeException e){
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * What happens when the socket is closed.
+   *
+   * @param session session info
+   */
   @OnClose
   public void onClose(Session session) throws IOException {
     // WebSocket connection closes
@@ -77,14 +163,23 @@ public class Server {
     sessionUsernameMap.remove(session);
     usernameSessionMap.remove(username);
 
+    /*TODO
+     * Send stats here
+     */
+
     String message = username + " disconnected";
     broadcast(message);
   }
 
-  // TODO probably add some more stuff to end the game and tell the user things are borked
   @OnError
   public void onError(Session session, Throwable throwable) {
     // Do error handling here
     logger.info("Entered into Error");
+    try{
+      broadcast("An error was encountered. Sorry this is so undescriptive :(");
+    }
+    catch(IOException e) {
+      logger.info("broadcast() had an IOException inside of error");
+    }
   }
 }
