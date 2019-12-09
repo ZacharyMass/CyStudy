0ee314adac5528b043a14863a743273f0a8fb2a7
Zach Gorman <gorman.zacharyj@gmail.com>
2019-12-08 20:14:41 -0600

    added endpoint to add time studied

--- BackEnd/src/main/java/com/jr7/cystudy/controller/StatsController.java
+++ BackEnd/src/main/java/com/jr7/cystudy/controller/StatsController.java
@@ -3,9 +3,7 @@ package com.jr7.cystudy.controller;
 import com.jr7.cystudy.model.Stats;
 import com.jr7.cystudy.service.StatsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StatsController {
@@ -15,5 +13,10 @@ public class StatsController {
   @GetMapping(path = "/get-users-stats")
   public Stats getUsersStats(@RequestParam String usrname, @RequestParam String className) {
     return statsServ.getUsersStats(usrname, className);
+  }
+
+  @PostMapping(path = "/add-time")
+  public String addTime(@RequestBody String username, @RequestBody String className, @RequestBody float t){
+    return statsServ.addTime(className, username, t);
   }
 }
