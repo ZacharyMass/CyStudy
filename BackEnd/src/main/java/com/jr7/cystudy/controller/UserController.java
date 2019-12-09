5f51d24d05d8b0d838ef61a306ab802be5866449
Zach Gorman <gorman.zacharyj@gmail.com>
2019-12-08 19:32:02 -0600

    redoing imports

--- BackEnd/src/main/java/com/jr7/cystudy/controller/UserController.java
+++ BackEnd/src/main/java/com/jr7/cystudy/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.jr7.cystudy.controller;
 
 import com.jr7.cystudy.model.User;
-import com.jr7.cystudy.service.*;
+import com.jr7.cystudy.service.UserService;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controller for everything related to User models.
